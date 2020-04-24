08dcfd75db8933499e3c407943f5a5c1628d96b8
Kirk Roerig <mr.possoms@gmail.com>
2020-04-23 19:34:57 -0600

    Initial commit

---
+++ game.server.js
@@ -0,0 +1,32 @@
+const g = require('./static/js/g.js');
+
+module.exports.server = {
+	// map of all connected players
+	players: {},
+	// complete game state
+	state: {},
+	// handlers for all player connection events
+	player: {
+		connected: function(player)
+		{
+			console.log('player: ' + player.id + ' connected');
+		},
+		on_message: function(player, message)
+		{
+			console.log('player: ' + player.id + ' on_message');
+		},
+		update: function(player)
+		{
+
+		},
+		disconnected: function(player)
+		{
+			console.log('player: ' + player.id + ' disconnected');
+		}
+	},
+	// main game loop
+	update: function(dt)
+	{
+
+	}
+};
